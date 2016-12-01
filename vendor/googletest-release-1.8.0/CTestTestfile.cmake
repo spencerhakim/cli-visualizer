1dd06d9c9159085f89a793c0398eddd1eef0dc64
Spencer Hakim <spencer.hakim@gmail.com>
2016-12-01 13:25:25 -0500

    Accidentally committed these

--- vendor/googletest-release-1.8.0/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/spencerh/Code/cli-visualizer/vendor/googletest-release-1.8.0
-# Build directory: /Users/spencerh/Code/cli-visualizer/vendor/googletest-release-1.8.0
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- vendor/googletest-release-1.8.0/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/spencerh/Code/cli-visualizer/vendor/googletest-release-1.8.0/googletest
-# Build directory: /Users/spencerh/Code/cli-visualizer/vendor/googletest-release-1.8.0/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
